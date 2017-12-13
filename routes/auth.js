5225f02aba1e10dde754da080dd364fe0e0aa6e7
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-12-13 22:00:19 +0800

    change auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,23 @@
 const passport = require('passport');
 module.exports = app => {
 
-  app.get('/', (req, res, next) => {
-    res.render('index', {title: 'Index || RateMe'});
+  app.get('/', (req, res) => {
+    res.send({ bye: 'buddy' });
   });
 
-  app.get('/signup', (req, res) => {
-    res.render('user/signup', {title: 'Sign Up || RateMe'});
-  });
+  // app.get('/', (req, res, next) => {
+  //   res.render('index', {title: 'Index || RateMe'});
+  // });
 
-  app.post('/signup', passport.authenticate('local.signup',{
-    successRedirect: '/home',
-    failureRedirect: '/signup',
-    failureFlash : true
-  }));
+  // app.get('/signup', (req, res) => {
+  //   res.render('user/signup', {title: 'Sign Up || RateMe'});
+  // });
+
+  // app.post('/signup', passport.authenticate('local.signup',{
+  //   successRedirect: '/home',
+  //   failureRedirect: '/signup',
+  //   failureFlash : true
+  // }));
 
   app.get('/login', (req, res) => {
     res.render('user/login', {title: 'Login || RateMe'});
