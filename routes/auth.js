77941ec57be877265c1c9fb831ff8f3e04b4fd80
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2017-12-18 01:08:55 +0800

    change auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -45,10 +45,6 @@ module.exports = app => {
   app.get('/api/current_user', (req, res)=>{
     res.send(req.user);
   });
-
-  app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-  });
 };
 
 
